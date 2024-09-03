2a87e9c4eea164012414c88a50bc7d2ab6a1ad39
LittleW <w-army@hotmail.co.th>
2024-09-03 09:29:57 +0700

    first init

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
