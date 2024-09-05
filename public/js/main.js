f66845b774e2b35bcddd23ce66ee21f1b1e0ce75
LittleW <w-army@hotmail.co.th>
2024-09-05 20:52:14 +0700

    make functional menu

--- public/js/main.js
+++ public/js/main.js
@@ -2,13 +2,33 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
-//difficulty 1 for easy, 2 for medium, 3 for hard
+function handleDifficultyChange(difficultyValue) {
+    console.log('Updated difficulty:', difficultyValue);
+}
 
-let playerHealth = Math.max(5, 11 - difficulty**2); // easy: 10, medium: 7, hard: 5
-let monsterDamage = difficulty + 1
+function enterGame() {
+  $('section.start').css({"display": "none"});
+  $('section.game').css({"display": "block"});
+}
 
-console.log(playerHealth);
 
-score = parseInt($('h1.score span').text());
-console.log(score);
 
+$('section.start .start-btn').on('click', function () {
+    if (difficulty == undefined) {
+      //difficulty 1 for easy, 2 for medium, 3 for hard
+      difficulty = handleDifficultyChange(difficulty);
+      console.log(difficulty);
+
+      // alert
+      $('section.start .alert').css({"display" : "block"});
+    } else {
+      console.log("select: ", difficulty);
+      enterGame()
+      let playerHealth = Math.max(5, 11 - difficulty**2); // easy: 10, medium: 7, hard: 5
+      let monsterDamage = difficulty + 1
+      console.log(playerHealth);
+      
+      score = parseInt($('h1.score span').text());
+      console.log(score);
+    }
+});
--- public/js/ui.js
+++ public/js/ui.js
@@ -1,5 +1,9 @@
+var difficulty; // declare globally
+
 $('input[name="difficulty"]').on('change', function() {
     var selectedValue = $(this).next('label').text();
     $('.difficulty-text').text(selectedValue);
-    var difficulty = $(this).val();
+    
+    difficulty = $(this).val(); // This now updates the global difficulty
+    handleDifficultyChange(difficulty); // pass the updated value to the handler
 });
