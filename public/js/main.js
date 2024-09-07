f5a798e11fad88532004145922af5b0b54b1e7bb
LittleW <w-army@hotmail.co.th>
2024-09-07 19:43:30 +0700

    make createMonsters, stageMultiplier, and speedStage

--- public/js/main.js
+++ public/js/main.js
@@ -15,7 +15,12 @@ function gameInit(difficulty) {
   let playerHealth = Math.max(5, 11 - difficulty**2); // easy: 10, medium: 7, hard: 5
   let minimumMonsterDamage = difficulty + 1;
   let stage = 1;
-  return {playerHealth, minimumMonsterDamage, stage, difficulty}
+  let multiplier = stage * difficulty;
+  return {playerHealth, minimumMonsterDamage, stage, difficulty, multiplier};
+}
+
+function stageMultiplier(stage, difficulty) {
+  return stage * difficulty;
 }
 
 function getScore() {
@@ -23,15 +28,13 @@ function getScore() {
 }
 
 function totalMonsterOnStage(stage,  difficulty) {
-  // Multiplier
-  let totalMonster = Math.ceil(stage*4.5) + Math.floor(1.5*difficulty);
-  return totalMonster;
+  // total monsters in stage
+  return Math.ceil(stage*4.5) + Math.floor(1.5*difficulty);
 }
 
-function spawnMonster(stage, difficulty) {
+function spawnMonster(multiplier) {
   // Spawn both circle and square here uwu
   // Spawn one time per function
-  let multiplier = stage * difficulty
   const type = Math.random() < (0.8 ** multiplier) ? 1 : 2; // 80% circle:1, 20% square:2
   // Spawn on random tile
   const spawn = getRandomInt(9) + 1;
@@ -57,17 +60,54 @@ function appearMonster(monster, tile) {
     shape = 'square';
   }
 
+  // Change the normal square to red
   if (monster.mutate == 1){
     $(`.hole:nth-child(${tile}) .bg.e .entity`).css({"--color": "red"});
   }
 
+  // Show Monster
   $(`.hole:nth-child(${tile}) .bg.e .entity`).addClass(shape);
-  $(`.hole:nth-child(${tile}) .bg.e`).css({
-      "display": "block"});
+  $(`.hole:nth-child(${tile}) .bg.e`).css({"display": "block"});
+
+  // Show Hole
+  $(`.hole:nth-child(${tile}) .bg.h`).css({"display": "block"});
 }
 
 function disappearMonster(monster, tile) {
-// Reset color, shape, type
+  // Reset color, shape, type
+
+  // Change entity shape from type
+  let shape;
+  if (monster.typeMons == 1) {
+    shape = 'circle';
+  } else {
+    shape = 'square';
+  }
+  $(`.hole:nth-child(${tile}) .bg.e .entity`).removeClass(shape);
+  
+  // default color
+  if (monster.mutate == 1){
+    $(`.hole:nth-child(${tile}) .bg.e .entity`).css({"--color": "white"});
+  }
+
+  // hide monster and hole
+  $(`.hole:nth-child(${tile}) .bg.e`).css({"display": "none"});
+  $(`.hole:nth-child(${tile}) .bg.h`).css({"display": "none"});
+}
+
+function speedStage (multiplier) {
+  // monster spawn delay
+  return 2.5 - (0.15 * multiplier)
+}
+
+function createMonster(player) {
+  let monsters = [];
+  let totalMonster = totalMonsterOnStage(player.stage, player.difficulty);
+  // making monster and then add to array
+  for (let i = 0; i < totalMonster; i++) {
+    monsters.push(spawnMonster(player.multiplier));
+  };
+  return monsters
 }
 
 $('section.start .start-btn').on('click', function () {
@@ -83,12 +123,7 @@ $('section.start .start-btn').on('click', function () {
       enterGame();
       var player = gameInit(difficulty);
 
-      var monsters = [];
-      let totalMonster = totalMonsterOnStage(player.stage, player.difficulty);
-      // making monster and then add to array
-      for (let i = 0; i < totalMonster; i++) {
-        monsters.push(spawnMonster(player.stage, player.difficulty));
-      };
+      var monsters = createMonster(player);
       console.log('monsters: ',monsters);
     }
 });
